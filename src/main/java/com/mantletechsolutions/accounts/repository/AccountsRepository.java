9b5c170d24f6ab99fe18a525cc1bfbe0dddc759b
akanimo sam okure <akan.okure@gmail.com>
2026-05-02 07:26:42 +0100

    add repository annotation

--- src/main/java/com/mantletechsolutions/accounts/repository/AccountsRepository.java
+++ src/main/java/com/mantletechsolutions/accounts/repository/AccountsRepository.java
@@ -2,6 +2,8 @@ package com.mantletechsolutions.accounts.repository;
 
 import com.mantletechsolutions.accounts.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
 }
--- src/main/java/com/mantletechsolutions/accounts/repository/CustomerRepository.java
+++ src/main/java/com/mantletechsolutions/accounts/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.mantletechsolutions.accounts.repository;
 
 import com.mantletechsolutions.accounts.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
