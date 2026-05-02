19e53e3dc219eec01e7ad8d26657d4bb7ed47ab8
akanimo sam okure <akan.okure@gmail.com>
2026-05-02 09:05:23 +0100

    implement CreateAccounts method in AcountsController

--- src/main/java/com/mantletechsolutions/accounts/controller/AccountsController.java
+++ src/main/java/com/mantletechsolutions/accounts/controller/AccountsController.java
@@ -1,7 +1,9 @@
 package com.mantletechsolutions.accounts.controller;
 
+import com.mantletechsolutions.accounts.constants.AccountsConstants;
 import com.mantletechsolutions.accounts.dto.CustomerDto;
 import com.mantletechsolutions.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountsController {
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
-
+    return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
 }
