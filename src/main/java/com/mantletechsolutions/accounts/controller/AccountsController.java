5f47acaddf581996b9101cdd575a86f3a7c5c070
akanimo sam okure <akan.okure@gmail.com>
2026-05-04 09:10:15 +0100

    Update Accounts controller fetch implementation

--- src/main/java/com/mantletechsolutions/accounts/controller/AccountsController.java
+++ src/main/java/com/mantletechsolutions/accounts/controller/AccountsController.java
@@ -27,7 +27,7 @@ public class AccountsController {
 
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails (@RequestParam String mobileNumber){
-
-        return null;
+      CustomerDto customerDto =  iAccountsService.fetchAccount(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 }
