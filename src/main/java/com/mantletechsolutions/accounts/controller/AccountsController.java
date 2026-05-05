11333f1e8335e3d593fd797dc42292042b10e88b
akanimo sam okure <akan.okure@gmail.com>
2026-05-05 07:22:40 +0100

    Implement delete endpoint in account controller

--- src/main/java/com/mantletechsolutions/accounts/controller/AccountsController.java
+++ src/main/java/com/mantletechsolutions/accounts/controller/AccountsController.java
@@ -19,38 +19,51 @@ public class AccountsController {
     private IAccountsService iAccountsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
-   iAccountsService.createAccount(customerDto);
-    return ResponseEntity
-            .status(HttpStatus.CREATED)
-            .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        iAccountsService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto> fetchAccountDetails (@RequestParam String mobileNumber){
-      CustomerDto customerDto =  iAccountsService.fetchAccount(mobileNumber);
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+        CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
     @PutMapping("/update")
 
-    public ResponseEntity<ResponseDto> updateAccountDetails( @RequestBody CustomerDto customerDto){
-       boolean isUpdated =   iAccountsService.updateAccount(customerDto);
-       if ( isUpdated) {
+    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if (isUpdated) {
 
-           return ResponseEntity
-                   .status(HttpStatus.OK)
-                   .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
-       }
-           else {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        } else {
 
-          return     ResponseEntity
-                       .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                       .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
-           }
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+        }
 
-
-       }
 
     }
+
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
+        boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
+        if (isDeleted) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        } else {
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+        }
+
+    }
+}
 
