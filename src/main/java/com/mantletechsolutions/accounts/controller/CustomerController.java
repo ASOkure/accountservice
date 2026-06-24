6f7e788041c758a77754f32563197a87133981af
akanimo sam okure <akan.okure@gmail.com>
2026-06-24 22:56:28 +0100

    add logger

--- src/main/java/com/mantletechsolutions/accounts/controller/CustomerController.java
+++ src/main/java/com/mantletechsolutions/accounts/controller/CustomerController.java
@@ -11,6 +11,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
 import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -27,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
 public class CustomerController {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
     private final ICustomersService iCustomersService;
 
