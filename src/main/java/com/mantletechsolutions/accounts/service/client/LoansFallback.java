cde67469fbacfa0725685684cbad8fc0d4147f06
akanimo sam okure <akan.okure@gmail.com>
2026-07-07 22:11:51 +0100

    and and implement CardsFallback and LoansFallback

---
+++ src/main/java/com/mantletechsolutions/accounts/service/client/LoansFallback.java
@@ -0,0 +1,17 @@
+package com.mantletechsolutions.accounts.service.client;
+
+import com.mantletechsolutions.accounts.dto.LoansDto;
+import com.mantletechsolutions.accounts.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient{
+
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+
+
+}
